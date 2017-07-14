523e24afc206fbcb7ee3a52a7e3e61a394db37c4
PCi7-home <luis.santamaria.tpp@gmail.com>
2017-07-14 12:20:38 -0500

    subiendo iprosap

--- app/model/util/config.php
+++ app/model/util/config.php
@@ -1,14 +1,13 @@
 <?php
-    define("HOST", "localhost");
+    /*define("HOST", "localhost");
     define("USER", "root");
     define("PASS", "admin");
-    define("DB", "integradb");
+    define("DB", "integradb");*/
 
 
 /*Datos Hostinguer !!!*/
 
-    /*define("HOST", "mysql.hostinger.es");
+    define("HOST", "mysql.hostinger.es");
     define("USER", "u368158266_creat");
     define("PASS", "t1cl4v1lc@.");
     define("DB", "u368158266_ipro");
-*/
